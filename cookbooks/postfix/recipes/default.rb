5b96fa7e9f0bbcd93ad4d38d4d6877c5346ed8b2
Stuart Watson <stuart.watson@unep-wcmc.org>
2013-01-22 10:26:47 +0000

    First Commit

---
+++ cookbooks/postfix/recipes/default.rb
@@ -0,0 +1,46 @@
+bash "Install Postfix" do
+user "root"
+code <<-EOH
+sudo apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y install postfix
+EOH
+end
+
+service "postfix" do
+service_name "postfix"
+supports :restart => true, :status => true, :reload => true
+end
+
+
+template "/etc/postfix/main.cf" do
+source "main.cf.erb"
+mode 0600
+notifies :restart, resources(:service => "postfix"), :immediately
+end
+
+template "/etc/postfix/sasl_passwd" do
+source "sasl_passwd.erb"
+mode 0600
+end
+
+template "/etc/postfix/generic" do
+source "generic.erb
+mode 0600
+end
+
+
+bash "Postfix configuration" do
+user "root"
+code <<-EOH
+sudo postmap /etc/postfix/sasl_passwd
+cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem|sudo tee -a /etc/postfix/cacert.pem
+sudo newaliases
+sudo postmap /etc/postfix/generic
+EOH
+end
+
+
+service "postfix" do
+service_name "postfix"
+supports :restart => true, :status => true, :reload => true
+end
